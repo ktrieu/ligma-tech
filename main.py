c3bcf8cdd9f8438b4f30d23d8c1ce821b509b115
Kevin Trieu <kevin.trieu5813@gmail.com>
2020-12-24 02:53:29 -0500

    Add basic proxy to Figma website

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from aiohttp import web
+from aiohttp import web, ClientSession
+
+session = ClientSession()
+
+FIGMA_URL = "https://figma.com"
 
 
 async def handle(request):
-    print(request)
-    return web.Response(text="HAIL SATAN")
+    figma_response = await session.get(f"{FIGMA_URL}{request.rel_url}")
+    if figma_response.content_type == "text/html":
+        return web.Response(body=await figma_response.text(), content_type="text/html")
+    else:
+        response_content = await figma_response.read()
+        return web.Response(
+            body=response_content, content_type=figma_response.content_type,
+        )
 
 
 if __name__ == "__main__":
     app = web.Application()
-    app.add_routes([web.get("/", handle)])
+    app.add_routes([web.get("/{tail:.*}", handle)])
     web.run_app(app)
